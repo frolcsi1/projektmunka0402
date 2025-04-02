a925ed456b1a296370f90885101d68736fa09940
frolcsi1 <frolcsi1@gmail.com>
2025-04-02 20:11:52 +0000

    Végső változtatások

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function scrollToTarget(event, targetId) {
+function scrollToTarget(event, target, targetId) {
     event.preventDefault();
+    const element = document.querySelector(target);
+    if (!element.classList.contains('show')) {
+        element.classList.add('show');
+    }
     const targetElement = document.querySelector(targetId);
     if (targetElement) {
         targetElement.scrollIntoView({ behavior: 'smooth' });
